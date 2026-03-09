f581a51337c8fbbb2496402a90040ed212c934f8
Bryan Mendoza <bmendoza.dev@hotmail.com>
2026-03-09 02:09:37 -0600

    refactor: remove unused date formatter and UTC date utilities

--- src/shared/utils/utc-dates/get-date/get-date.mod.server.ts
+++
@@ -1,8 +0,0 @@
-import { UTCDateMini } from "@date-fns/utc";
-
-/**
- * @param {Date} date - The date to be converted to UTC.
- *
- * If no date is provided, the current date is used.
- */
-export const getUTCDate = (date: Date = new Date()) => new UTCDateMini(date);
